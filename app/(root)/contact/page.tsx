d96ad300ebb648c86e58d0cb91d514b1e8f6db2c
aryan7412 <aryan7412ss@gmail.com>
2025-07-07 15:06:17 +0530

    Added Contact Us Page

---
+++ app/(root)/contact/page.tsx
@@ -0,0 +1,13 @@
+import Contact from "@/components/ContactUs"
+import FAQ from "@/components/FAQ"
+
+const page = () => {
+  return (
+    <div>
+        <Contact />
+        <FAQ />
+    </div>
+  )
+}
+
+export default page
