902645374a69f30ccd9717000e92215ec2562c28
aryan7412 <aryan7412ss@gmail.com>
2025-07-08 10:52:35 +0530

    Added about and contact pages

---
+++ app/(root)/about/page.tsx
@@ -0,0 +1,13 @@
+import AboutUs from "@/components/AboutUs"
+import FAQ from "@/components/FAQ"
+
+const page = () => {
+  return (
+    <div>
+        <AboutUs />
+        <FAQ />
+    </div>
+  )
+}
+
+export default page
